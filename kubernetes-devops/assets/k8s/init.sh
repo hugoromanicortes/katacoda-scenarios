40e098a4c7e19d2923ecc576874a626b53b37731
A457453 <hugo.romani.cortes@atos.net>
2020-04-28 00:08:07 +0200

    update

--- kubernetes-devops/assets/k8s/init.sh
+++ kubernetes-devops/assets/k8s/init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 `kubeadm init --token=102952.1a7dd4cc8d1f4cc5`
 
-`sudo cp /etc/kubernetes/admin.conf $HOME/ & sudo chown $(id -u):$(id -g) $HOME/admin.conf & export KUBECONFIG=$HOME/admin.conf`
+`sudo cp /etc/kubernetes/admin.conf $HOME/`
+`sudo chown $(id -u):$(id -g) $HOME/admin.conf`
+export KUBECONFIG=$HOME/admin.conf`
