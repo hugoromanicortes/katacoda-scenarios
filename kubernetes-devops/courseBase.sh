3a0ee752fb5ab880bc42ed6f9ce1519d4799a2e6
A457453 <hugo.romani.cortes@atos.net>
2020-04-27 23:51:26 +0200

    update init k8s

--- kubernetes-devops/courseBase.sh
+++
@@ -1,6 +0,0 @@
-ssh root@[[HOST1_IP]] "kubeadm init --token=102952.1a7dd4cc8d1f4cc5"
-sleep 60
-ssh root@[[HOST2_IP]] "kubeadm reset"
-ssh root@[[HOST2_IP]] "kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 [[HOST_IP]]:6443"
-
-ssh root@[[HOST1_IP]] "sudo cp /etc/kubernetes/admin.conf $HOME/ & sudo chown $(id -u):$(id -g) $HOME/admin.conf & export KUBECONFIG=$HOME/admin.conf"
