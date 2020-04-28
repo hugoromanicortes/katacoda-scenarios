93dccc9fbf03697eebb86311983bf90526d0b4a1
A457453 <hugo.romani.cortes@atos.net>
2020-04-28 22:31:23 +0200

    update

---
+++ kubernetes-devops/assets/courseBase.sh
@@ -0,0 +1,1 @@
+ssh root@host01 "kubeadm init --token=102952.1a7dd4cc8d1f4cc5; sudo cp /etc/kubernetes/admin.conf $HOME/admin.conf; sudo chown $(id -u):$(id -g) $HOME/admin.conf; export KUBECONFIG=$HOME/admin.conf;"
